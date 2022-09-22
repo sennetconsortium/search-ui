2c3bbb6e47901eb92820872fc5d22b63ab8cb69c
maxsibilla <maxsibilla@icloud.com>
2022-09-22 12:29:36 -0400

    Reorganizing footer

--- src/components/custom/layout/AppFooter.js
+++ src/components/custom/layout/AppFooter.js
@@ -7,12 +7,18 @@ const AppFooter = ({isFixedBottom}) => {
         <Container>
             <footer className={`py-3 my-4 ${fixedBottom}`}>
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li className="nav-item"><a href="https://sennetconsortium.org/" className="nav-link px-2 text-muted">Home</a></li>
-                    <li className="nav-item"><a href="https://sennetconsortium.org/involvement/" className="nav-link px-2 text-muted">Involvement</a></li>
-                    <li className="nav-item"><a href="https://sennetconsortium.org/about-2/" className="nav-link px-2 text-muted">About</a></li>
-                    <li className="nav-item"><a href="https://sennetconsortium.org/contact" className="nav-link px-2 text-muted">Contact</a></li>
+                    <li className="nav-item"><a href="https://sennetconsortium.org/"
+                                                className="nav-link px-2 text-muted">SenNet Consortium</a></li>
+                    <li className="nav-item"><a href="https://sennetconsortium.org/about-2/"
+                                                className="nav-link px-2 text-muted">About</a></li>
+                    <li className="nav-item"><a href="https://sennetconsortium.org/involvement/"
+                                                className="nav-link px-2 text-muted">Involvement</a></li>
+                    <li className="nav-item"><a href="https://sennetconsortium.org/contact"
+                                                className="nav-link px-2 text-muted">Contact</a></li>
                 </ul>
-                <div className="text-center text-muted"><a href="https://commonfund.nih.gov/senescence" className={'nav-link px-2 text-muted'}>NIH Common Fund</a></div>
+                <div className="text-center text-muted"><a href="https://commonfund.nih.gov/senescence"
+                                                           className={'nav-link px-2 text-muted'}>NIH Common Fund</a>
+                </div>
             </footer>
         </Container>
     );
