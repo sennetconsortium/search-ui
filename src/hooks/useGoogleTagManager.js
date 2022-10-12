ae24a27f3af5c504f958efe2f2a0c20c5473450e
Lisa-Ann Bruney <lisabruney@pitt.edu>
2022-10-12 16:18:33 -0400

    Add custom Google Tag Manager hook - #95

---
+++ src/hooks/useGoogleTagManager.js
@@ -0,0 +1,42 @@
+import { useEffect } from 'react'
+
+function useGoogleTagManager() {
+    let gtm
+    let noscript
+
+    // TODO: Ideally we want to set this value based on environment, i.e. dev | live etc. So this line must change
+    const id = 'GTM-WMT659V'
+
+    const className = 'js-gtm'
+
+    useEffect(() => {
+        // Double check that this hasn't been added already
+        if (document.getElementsByClassName(className).length) return
+
+        // GTM Tag
+        gtm = document.createElement('script')
+        gtm.classList.add(className)
+        gtm.async = true
+
+        gtm.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${id}');`
+        document.head.appendChild(gtm)
+
+        // Add GTM No JavaScript
+        noscript = document.createElement('noscript')
+        noscript.async = true
+        noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${id}"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        document.body.prepend(noscript)
+
+        return () => {
+            document.head.removeChild(gtm)
+        }
+    }, [gtm])
+    return { gtm }
+}
+
+export default useGoogleTagManager
