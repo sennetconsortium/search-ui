a94ad183f7e92597ce50eabac1ca80dc88e27775
Lisa-Ann B <lisabruney@pitt.edu>
2022-10-14 12:57:58 -0400

    Add methods for tracking of result item and link clicks - #95

--- src/components/custom/js/addons/GoogleTagManager.js
+++ src/components/custom/js/addons/GoogleTagManager.js
@@ -9,12 +9,14 @@ class GoogleTagManager extends Addon {
     }
 
     extractEvent() {
+        if (this.el == null || !this.el.length) return
         const _t = this
         this.el[0].classList.forEach((val) => {
             if (val.indexOf(_t.app) !== -1) {
                 _t.event = val.split('--')[1]
             }
         })
+        this.log(`Google Tag manager ... ${this.event}`)
     }
 
     modules() {
@@ -24,6 +26,9 @@ class GoogleTagManager extends Addon {
                 break
             case 'facets':
                 this.facets()
+                break
+            case 'results':
+                this.results()
                 break
             default:
                 this.links()
@@ -53,7 +58,7 @@ class GoogleTagManager extends Addon {
 
     facets() {
         const _t = this
-        this.group = this.el.find('.sui-facet__title').text()
+        this.group = this.el.parent().find('.sui-facet__title').text()
         this.el.on(
             'click',
             '.sui-multi-checkbox-facet__option-input-wrapper',
@@ -63,7 +68,34 @@ class GoogleTagManager extends Addon {
         )
     }
 
-    links() {}
+    handleResults(e) {
+        const tr = this.currentTarget(e)
+        const th = ['created_by', 'sennet_id', 'entity', 'lab_id', 'category', 'group']
+        const data = {}
+        for (let i = 0; i < th.length; i++) {
+            data[th[i]] = tr.find('td').eq(i).text()
+        }
+        this.gtm(data)
+    }
+
+    results() {
+        const _t = this
+        this.el.on('click', 'tbody tr', (e) => {
+            _t.handleResults(e)
+        })
+    }
+
+    handleLinks(e) {
+        this.event = 'links'
+        this.gtm({link: this.currentTarget(e).text()})
+    }
+
+    links() {
+        const _t = this
+        $('a').on('click', (e) => {
+            _t.handleLinks(e)
+        })
+    }
 
     gtm(args) {
         const data = { event: this.event, ...args }
