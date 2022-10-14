4a37c7fcfb0f1128e4741060f7694ed8c7e28050
Lisa-Ann B <lisabruney@pitt.edu>
2022-10-14 13:00:15 -0400

    Put useEffect for addon initialization in _app - #95

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,21 @@
 import '../public/css/main.css'
 import log from 'loglevel'
 import ErrorBoundary from '../components/custom/error/ErrorBoundary'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useIdleTimer } from 'react-idle-timer'
 import { deleteCookie, setCookie } from 'cookies-next'
 import { getIngestEndPoint, IDLE_TIMEOUT } from '../config/config'
 import useGoogleTagManager from '../hooks/useGoogleTagManager'
+import addons from "../components/custom/js/addons/addons"
 
 function MyApp({ Component, pageProps }) {
     const router = useRouter()
     useGoogleTagManager()
+
+    useEffect(() =>{
+        addons('init')
+    }, [])
 
     const onIdle = () => {
         setCookie('isAuthenticated', false)
@@ -24,6 +30,7 @@ function MyApp({ Component, pageProps }) {
 
     // log.enableAll()
     log.setLevel('debug')
+    
     return (
         <ErrorBoundary>
             <Component {...pageProps} />
