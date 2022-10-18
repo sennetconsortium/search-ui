7f5b59d694cb0c0993020063a5390c0eb8deb4de
maxsibilla <maxsibilla@icloud.com>
2022-10-18 13:08:17 -0400

    Rebasing branch with dev-integrate

--- src/hooks/useGoogleTagManager.js
+++ src/hooks/useGoogleTagManager.js
@@ -1,11 +1,11 @@
 import { useEffect } from 'react'
+import { getGoogleTagManagerId } from '../config/config'
 
 function useGoogleTagManager() {
     let gtm
     let noscript
 
-    // TODO: Ideally we want to set this value based on environment, i.e. dev | live etc. So this line must change
-    const id = 'GTM-WMT659V'
+    const id = getGoogleTagManagerId() || 'GTM-WMT659V'
 
     const className = 'js-gtm'
 
@@ -18,18 +18,15 @@ function useGoogleTagManager() {
         gtm.classList.add(className)
         gtm.async = true
 
-        gtm.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${id}');`
+        gtm.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                         f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','${id}');`
         document.head.appendChild(gtm)
 
         // Add GTM No JavaScript
         noscript = document.createElement('noscript')
         noscript.async = true
-        noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${id}"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${id}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
         document.body.prepend(noscript)
 
         return () => {
