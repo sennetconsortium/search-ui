58a2fe3717a3b9012a96c4a509dcccd77d6e8775
Lisa-Ann B <lisabruney@pitt.edu>
2022-10-18 10:01:16 -0400

    Use AppContext and PropTypes in Spinner component - #98

--- src/components/custom/Spinner.jsx
+++ src/components/custom/Spinner.jsx
@@ -1,13 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import { useContext } from 'react'
+import PropTypes from 'prop-types'
+import 'bootstrap/dist/css/bootstrap.css'
+import AppContext from '../../context/AppContext'
 
-function Spinner() {
+function Spinner({ text }) {
+    const { _t } = useContext(AppContext)
+
     return (
         <div className="text-center p-3">
-            <span>Loading, please wait...</span>
+            <span>{_t(text)}</span>
             <br></br>
             <span className="spinner-border spinner-border-lg align-center alert alert-info"></span>
         </div>
     )
+}
+
+Spinner.defaultProps = {
+    text: 'Loading, please wait...',
+}
+
+Spinner.propTypes = {
+    text: PropTypes.string,
 }
 
 export default Spinner
