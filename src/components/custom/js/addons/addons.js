05968d427f16e0cdfaad3ce2b81fbddc487c57cc
Lisa-Ann B <lisabruney@pitt.edu>
2022-10-25 13:16:38 -0400

    Reform to accomodate for args param - #136

--- src/components/custom/js/addons/addons.js
+++ src/components/custom/js/addons/addons.js
@@ -1,13 +1,14 @@
 import Addon from './Addon'
 import GoogleTagManager from './GoogleTagManager'
 import AppModal from './AppModal'
+import Facets from './Facets'
 
 /**
  * JS functionality which enhance site functionality, not necessarily part of the core.
  * @param {String} source
  * @returns
  */
-function addons(source) {
+function addons(source, args) {
     Addon.log('Addons started ...', 'log', 'red')
     if (window[source] !== undefined) {
         return
@@ -15,20 +16,19 @@ function addons(source) {
     window[source] = true
     let apps = {
         gtm: GoogleTagManager,
-        modal: AppModal
+        modal: AppModal, 
+        facets: Facets
     }
 
     setTimeout(() => {
         for (let key in apps) {
             document
-                .querySelectorAll(`[class*='js-${key}--']`)
+                .querySelectorAll(`[class*='js-${key}--'], [data-js-${key}]`)
                 .forEach((el) => {
-                    new apps[key](el, key)
+                    new apps[key](el, {key, data: args.data })
                 })
 
-            document.querySelectorAll(`[data-js-${key}]`).forEach((el) => {
-                new apps[key](el, key)
-            })
+            
         }
 
         // Default: Capture all link clicks. 
