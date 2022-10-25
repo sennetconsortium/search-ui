a4c72174439cef086f8b76f7fb25eee40c10767c
maxsibilla <maxsibilla@icloud.com>
2022-10-25 14:31:04 -0400

    Changing other to OT

--- src/config/config.js
+++ src/config/config.js
@@ -16,7 +16,7 @@ export function getAuth() {
     return ''
 }
 
-const nonSupportedRuiOrgans = ['AO', 'BD', 'BS', 'MU', 'Other']
+const nonSupportedRuiOrgans = ['AO', 'BD', 'BS', 'MU', 'OT']
 
 export function isOrganRuiSupported(organ) {
     return !nonSupportedRuiOrgans.includes(organ)
