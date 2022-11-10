c9126e177511baea32e44c617aa9752acc6a4b27
maxsibilla <maxsibilla@icloud.com>
2022-11-10 14:24:40 -0500

    Updating logic to allow acces to publishes/public entities

--- src/pages/api/json/[entity].js
+++ src/pages/api/json/[entity].js
@@ -1,17 +1,17 @@
 import {getRootURL} from "../../../config/config";
 import log from "loglevel";
-import {getCookie} from "cookies-next";
+import {getCookie, hasCookie} from "cookies-next";
 
 export default async function handler(req, res) {
     const uuid = req.query.uuid
     let auth = req.headers.authorization
+    let myHeaders = new Headers();
 
-    if (req.headers.authorization === undefined) {
+    if (req.headers.authorization === undefined && hasCookie("groups_token", {req, res})) {
         auth = "Bearer " + getCookie("groups_token", {req, res})
+        myHeaders.append("Authorization", auth);
     }
 
-    let myHeaders = new Headers();
-    myHeaders.append("Authorization", auth);
     myHeaders.append("Content-Type", "application/json");
     let requestOptions = {
         method: 'GET',
