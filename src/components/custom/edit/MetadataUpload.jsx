7311256a6223568e620ca2046aaaf0625ca40755
Lisa-Ann B <lisabruney@pitt.edu>
2023-01-30 13:54:38 -0500

    Add forwarder popup

--- src/components/custom/edit/MetadataUpload.jsx
+++ src/components/custom/edit/MetadataUpload.jsx
@@ -1,9 +1,10 @@
 import React, {useContext, useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
-import { Upload, CheckCircleFill, XCircleFill, Download} from "react-bootstrap-icons";
+import { Upload, CheckCircleFill, XCircleFill, Download, Envelope, ForwardFill} from "react-bootstrap-icons";
 import InputGroup from 'react-bootstrap/InputGroup';
 import {getIngestEndPoint} from "../../../config/config";
-
+import Swal from 'sweetalert2'
+import log from 'loglevel'
 
 function MetadataUpload({children}) {
     useEffect(() => {
@@ -12,7 +13,6 @@ function MetadataUpload({children}) {
     const [fileStatus, setFileStatus] = useState('')
     const [error, setError] = useState(null)
     const [success, setSuccess] = useState(false)
-
 
     const handleUpload = async (e) => {
         const upload = e.currentTarget.files[0]
@@ -55,6 +55,27 @@ function MetadataUpload({children}) {
         }
     }
 
+    const forwardReport = (e) => {
+        const forwarder = Swal.fire({
+            customClass: {
+                container: 'c-help',
+                title: 'c-help__title',
+                confirmButton: 'c-help__btn'
+            },
+            width: 700,
+            title: 'Forward The Report To Curator',
+            input: 'email',
+            inputLabel: 'Input the sender\'s email address',
+            inputPlaceholder: 'Enter your email address',
+            showCloseButton: true,
+            confirmButtonText: 'Okay'
+        })
+
+        forwarder.then((value) => {
+            log.debug(value)
+        })
+    }
+
     return (
         <div className={`c-metadataUpload`}>
             <InputGroup className="mb-3">
@@ -62,13 +83,19 @@ function MetadataUpload({children}) {
                 <label htmlFor='entity_metadata' className='btn btn-outline-primary rounded-0 mt-1 btn--fileUpload'>
                     Upload Metadata
                     <input onChange={handleUpload} type='file' id='entity_metadata' name='entity_metadata' />
-                    <Upload />
+                    <Upload size={12} />
                 </label>
-                <span className={`c-metadataUpload__fileInfo js-fileInfo ${error ? 'has-error' : ''}`}>
+                <span className={`c-metadataUpload__meta js-fileInfo ${error ? 'has-error ' : ''}`}>
                     {error && <XCircleFill color='#842029' />}
                     {success && <CheckCircleFill color='#0d6efd' />}
-                    <small onClick={downloadDetails} title={`${error ? 'Download error report' : ''}`}>{fileStatus} {error && <Download />}</small>
+                    <small role={error ? 'button' : null} onClick={downloadDetails} title={`${error ? 'Download error report' : ''}`}>{fileStatus} {error && <Download />}</small>
                 </span>
+                {error && <span role='button' className={`c-metadataUpload__meta c-metadataUpload__meta--fwd`} onClick={forwardReport} title={'Forward the error report to curator'}>
+                    <span>
+                        <small>Forward Error Report</small> <Envelope />
+                        <span><ForwardFill /></span>
+                    </span>
+                </span>}
             </InputGroup>
         </div>
     )
