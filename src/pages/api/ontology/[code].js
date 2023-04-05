bb9b0d8c671882ba310eb532252268f797bc7cf4
Lisa-Ann B <lisabruney@pitt.edu>
2023-04-05 12:06:35 -0400

    Use backup strategy

--- src/pages/api/ontology/[code].js
+++ src/pages/api/ontology/[code].js
@@ -9,27 +9,54 @@ export default async function handler(req, res) {
     const key = req.query.code
     try {
         const filePath = ONTOLOGY_CACHE_PATH + '/.ontology_' + key
+        const filePathBackUp = filePath + '_bk'
         log.debug('CACHING TO ...', filePath)
         let ontology
+        let ontologyBackUp
+
+        const createFile = async (filePath, data) => {
+            await fs.mkdir(path.dirname(filePath), {recursive: true}).then(function () {
+                fs.writeFile(filePath, JSON.stringify(data), 'utf8')
+            })
+        }
 
         try {
+            // This is always expected to exist at build time
             ontology = await fs.readFile(filePath, 'utf8')
-            ontology = JSON.parse(ontology)
-        } catch (e) {
-            log.debug(`ONTOLOGY API file ${filePath} doesn't exist, creating...`)
-            ontology = await get_onotology_valueset(key)
-            log.debug(`Ontology for ${ontology}`, ontology)
+            if (ontology) {
+                ontology = JSON.parse(ontology)
+            }
+
+            // Look for a backup then create (in catch) if it does not exist
+            ontologyBackUp = await fs.readFile(filePathBackUp, 'utf8')
+            if (ontologyBackUp) {
+                ontologyBackUp = JSON.parse(ontologyBackUp)
+            }
+
+        }  catch (e) {
+            log.debug(`ONTOLOGY API error encountered at ontology code ${key}: ${e}`)
             if (ontology && ontology.length) {
-                await fs.mkdir(path.dirname(filePath), {recursive: true}).then(function () {
-                    fs.writeFile(filePath, JSON.stringify(ontology), 'utf8')
-                })
+                // Create a backup of what's there already
+                await createFile(filePathBackUp, ontology)
+            } else {
+                log.debug(`ONTOLOGY API Unfortunately no cache exists ${filePath}`)
+
+                ontology = await get_onotology_valueset(key)
+                if (ontology && ontology.length) {
+                    log.debug(`ONTOLOGY API get_onotology_valueset obtained on ${key}`)
+                    await createFile(filePath, ontology)
+                }
             }
         }
 
-        if (ontology) {
+        if (ontology && ontology.length) {
             res.status(200).json(ontology)
         } else {
-            res.status(404).json({code: key})
+            if (ontologyBackUp && ontologyBackUp.length) {
+                res.status(200).json(ontologyBackUp)
+            } else {
+                res.status(404).json({code: key})
+            }
         }
     } catch (error) {
         console.error(`ONTOLOGY API`, error)
