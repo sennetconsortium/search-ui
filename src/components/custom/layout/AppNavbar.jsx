b96f963b45c3e1f1a35eef927cffa774e460c8c2
Lisa-Ann B <lisabruney@pitt.edu>
2023-04-06 14:10:05 -0400

    Save with submenu styling

--- src/components/custom/layout/AppNavbar.jsx
+++ src/components/custom/layout/AppNavbar.jsx
@@ -1,14 +1,23 @@
 import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
 import {APP_TITLE, getLogoutURL} from '../../../config/config'
 import {APP_ROUTES} from '../../../config/constants'
-import {useContext} from 'react'
+import {useContext, useEffect, useRef} from 'react'
 import styles from '../appNavbar.module.css'
 import logo from './sennet-logo.png'
 import Image from 'next/image'
 import AppContext from '../../../context/AppContext'
+import Addon from "../js/addons/Addon";
 
 const AppNavbar = ({hidden, signoutHidden}) => {
     const {_t, isLoggedIn, logout, cache} = useContext(AppContext)
+    const hasInit = useRef(false)
+
+    useEffect(()=>{
+        if (hasInit.current === false) {
+            hasInit.current = true
+            Addon.navBar()
+        }
+    })
 
     const handleSession = (e) => {
         e.preventDefault()
@@ -18,6 +27,23 @@ const AppNavbar = ({hidden, signoutHidden}) => {
             url = APP_ROUTES.logout //getLogoutURL()
         }
         window.location.replace(url)
+    }
+
+    const supportedMetadata = () => {
+        let supported = {}
+        supported[cache.entities.source] = {
+            categories: [
+                cache.sourceTypes['Mouse']
+            ]
+        }
+        supported[cache.entities.sample] = {
+            categories: [
+                cache.sampleCategories.Block,
+                cache.sampleCategories.Section,
+                cache.sampleCategories.Suspension,
+            ]
+        }
+        return supported
     }
 
     return (
@@ -46,7 +72,7 @@ const AppNavbar = ({hidden, signoutHidden}) => {
                             active={false}
                             variant={'primary'}
                             hidden={hidden}
-                            title={_t("Create an Entity")}
+                            title={_t("Register entity")}
                             id="nav-dropdown"
                         >
                             {Object.keys(cache.entities).map((entity) => (
@@ -60,14 +86,38 @@ const AppNavbar = ({hidden, signoutHidden}) => {
                             active={false}
                             variant={'primary'}
                             hidden={hidden}
-                            title="Bulk create entities"
+                            title="Bulk register entities"
                             id="nav-dropdown--bulkCreate">
-                            <NavDropdown.Item
-                                href="/edit/bulk?entity_type=source">Sources</NavDropdown.Item>
-                            <NavDropdown.Item
-                                href="/edit/bulk?entity_type=sample">Samples</NavDropdown.Item>
-                            <NavDropdown.Item
-                                href="/edit/bulk?entity_type=dataset">Datasets</NavDropdown.Item>
+                            {Object.keys(cache.entities).map((entity) => (
+                                <NavDropdown.Item key={entity} href={`/edit/bulk/${entity}?action=register`}>
+                                    {entity}s
+                                </NavDropdown.Item>
+                            ))}
+
+                        </NavDropdown>
+                        <NavDropdown
+                            active={false}
+                            variant={'primary'}
+                            hidden={hidden}
+                            title="Bulk upload metadata"
+                            id="nav-dropdown--bulkMetadata">
+                            {Object.keys(supportedMetadata()).map((entity, key) => (
+                                <div key={`dropdownItem-md-${entity}`}>
+                                { key !== 0 && <NavDropdown.Divider  /> }
+                                    <NavDropdown.Item className='dropdown-item is-heading dropdown-toggle' aria-controls={`submenu-md-${entity}`}>
+                                        {entity}s
+                                    </NavDropdown.Item>
+
+                                   <div className={'submenu'} id={`submenu-md-${entity}`}>
+                                       <NavDropdown.Divider />
+                                       {Object.entries(supportedMetadata()[entity].categories).map((type, typekey) => (
+                                           <NavDropdown.Item key={`submenuItem-md-${type}`} href={`/edit/bulk/${type}?action=metadata&category=${type}`} className={'is-subItem'}>
+                                               <span>{supportedMetadata()[entity].categories[typekey]}</span>
+                                           </NavDropdown.Item>
+                                       ))}
+                                   </div>
+                                </div>
+                            ))}
                         </NavDropdown>
                     </Nav>
                     <Nav>
