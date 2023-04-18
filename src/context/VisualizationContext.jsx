9af11100137720c0dd5c2ef598cffb70f18badd2
SamSedivy <sls325@pitt.edu>
2023-04-18 15:37:09 -0400

    Use the ontology data to determine primary assays; Show secondary dataset visualization files on the primary datasets; Hide vitessce for primary datasets with no secondary datasets

--- src/context/VisualizationContext.jsx
+++ src/context/VisualizationContext.jsx
@@ -13,9 +13,9 @@ export const VisualizationProvider = ({ children }) => {
     const [showExitFullscreenMessage, setShowExitFullscreenMessage] = useState(null)
     const [isPrimaryDataset, setIsPrimaryDataset] = useState(false)
 
-    const showVitessce = data_types => {
+    const showVitessce = (data_types, is_primary_dataset, immediate_descendants) => {
         const supportedVitessceDataTypes = ['snRNA-seq', 'scRNA-seq', 'CODEX']
-        return supportedVitessceDataTypes.some(d => data_types.includes(d))
+        return supportedVitessceDataTypes.some(d => data_types.includes(d)) && immediate_descendants !== 0
     }
 
     const expandVitessceToFullscreen = () => {
