4057e3e20386f54e74e28d48c1489485901d0f23
Lisa-Ann B <lisabruney@pitt.edu>
2023-05-10 10:41:37 -0400

    Update footer - #545

--- src/components/custom/layout/AppFooter.js
+++ src/components/custom/layout/AppFooter.js
@@ -13,21 +13,22 @@ const AppFooter = ({isFixedBottom}) => {
 
     let fixedBottom = isFixedBottom ? 'fixed-bottom' : ''
     return (
-        <div className={'sui-layout-body__inner bg--entityWhite border-top'}>
+        <div className={`footer-wrapper sui-layout-body__inner bg--entityWhite border-top fixed-bottom`}>
             <ToastContainer />
 
-            <footer className={`py-3 ${fixedBottom} `}>
+            <footer className={`py-3`}>
                 <Row>
-                    <Col sm={2}>
+                    <Col sm={2} className='col--placeholder'>
                         <p></p>
                     </Col>
-                    <Col sm={10}>
+                    <Col sm={10} className='col--main'>
                         <Row>
-                            <Col sm={4}>
+                            <Col sm={4} className='col--logos'>
                                 <ul className="nav pb-3 mb-3 nav--org" aria-label='Organization name'>
                                     <li className="nav-item">
                                         <a className="navbar-brand" href="https://sennetconsortium.org/">
                                             <Image
+                                                title='Senescence Network (SenNet)'
                                                 className="d-inline-block align-text-top"
                                                 src={logo}
                                                 width="150"
@@ -35,39 +36,40 @@ const AppFooter = ({isFixedBottom}) => {
                                             />
                                         </a>
                                     </li>
-                                </ul>
-                            </Col>
-                            <Col sm={8}>
-                                <ul className="nav pb-3 mb-3 nav--menu" aria-label='Menu'>
-
-                                    <li className="nav-item"><a href="https://sennetconsortium.org/about-2/"
-                                                                className="nav-link px-2 text-muted">About</a></li>
-                                    <li className="nav-item"><a href="https://sennetconsortium.org/involvement/"
-                                                                className="nav-link px-2 text-muted">Funded Research</a></li>
-                                    <li className="nav-item"><a href="https://sennetconsortium.org/contact"
-                                                                className="nav-link px-2 text-muted">Contact</a></li>
-                                </ul>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col sm={4}><div className=" text-muted">
-                                <ul className="nav pb-3 mb-3" >
-                                    <li className="nav-item">
+                                    <li className="nav-item mt-3">
                                         <a className="navbar-brand" href="https://commonfund.nih.gov/senescence">
                                             <Image
-                                                className="d-inline-block align-text-top"
+                                                className="d-inline-block align-text-top nih-logo"
+                                                title='National Institute of Health'
                                                 src={nih}
-                                                width="350"
                                                 alt={_t("NIH logo")}
                                             />
                                         </a>
                                     </li>
                                 </ul>
-
-
-
-                            </div></Col>
-
+                            </Col>
+                            <Col sm={2}>
+                                <ul className="pb-3 mb-3 nav--menu" aria-label='About Menu'>
+                                    <li className="nav-item"><span className='nav-item-h'>About</span></li>
+                                    <li className="nav-item"><a href="https://sennetconsortium.org/about-2/">Project Website</a></li>
+                                    <li className="nav-item"><a href="https://sennetconsortium.org/involvement/">Funded Research</a></li>
+                                    <li className="nav-item"><a href="https://docs.sennetconsortium.org/">Documentation</a></li>
+                                    <li className="nav-item"><a href="https://sennetconsortium.org/contact">Contact</a></li>
+                                </ul>
+                            </Col>
+                            <Col sm={2}>
+                                <ul className="pb-3 mb-3 nav--menu" aria-label='Policies Menu'>
+                                    <li className="nav-item"><span className='nav-item-h'>Policies</span></li>
+                                    <li className="nav-item"><a href="https://sennetconsortium.org/policies/">Overview</a></li>
+                                    <li className="nav-item"><a href="https://sennetconsortium.org/external-data-use/">Data Sharing</a></li>
+                                </ul>
+                            </Col>
+                            <Col sm={2}>
+                                <ul className="pb-3 mb-3 nav--menu" aria-label='Funding Menu'>
+                                    <li className="nav-item"><span className='nav-item-h'>Funding</span></li>
+                                    <li className="nav-item"><a className='lnk--ic' href="https://commonfund.nih.gov/senescence">NIH Common Fund <BoxArrowUpRight/></a></li>
+                                </ul>
+                            </Col>
                         </Row>
 
                     </Col>
