f9e43ad2f7a688ce8947371804495844c0226c6a
Tyler Madonna <tjm159@pitt.edu>
2023-06-02 15:21:58 -0400

    Change last_modified filter label to modification date

--- src/config/config.js
+++ src/config/config.js
@@ -232,7 +232,7 @@ export const config = {
                 uiType: 'daterange',
             },
             last_modified_timestamp: {
-                label: 'Last Modified Date',
+                label: 'Modification Date',
                 type: 'range',
                 field: 'last_modified_timestamp',
                 isExpanded: false,
