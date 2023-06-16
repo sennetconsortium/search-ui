856ba18679843e7f80da8af89660f91928bd0bf6
Lisa-Ann B <lisabruney@pitt.edu>
2023-06-16 11:41:16 -0400

    Update AppNavbar links

--- src/components/custom/layout/AppNavbar.jsx
+++ src/components/custom/layout/AppNavbar.jsx
@@ -5,6 +5,7 @@ import {useContext} from 'react'
 import logo from './sennet-logo.png'
 import Image from 'next/image'
 import AppContext from '../../../context/AppContext'
+import {equals} from "../js/functions";
 
 const AppNavbar = ({hidden, signoutHidden}) => {
     const {_t, isLoggedIn, logout, cache, supportedMetadata} = useContext(AppContext)
@@ -55,31 +56,36 @@ const AppNavbar = ({hidden, signoutHidden}) => {
                             title={_t("Register entity")}
                             id="nav-dropdown"
                         >
-                            {Object.keys(cache.entities).map((entity) => (
-                                <NavDropdown.Item key={entity} href={`/edit/${entity}?uuid=register`}>
-                                    {_t(entity)}
-                                </NavDropdown.Item>
-                            ))}
+                            {['Single', 'Bulk'].map((range, key) => (
+                                <div key={`dropdownItem-register-${range}`} id={`dropdownItem-register-${range}`}>
+                                    {key !== 0 && <NavDropdown.Divider/>}
+                                    <NavDropdown.Item className='dropdown-item is-heading'
+                                                      aria-controls={`submenu-md-${range}`}>
+                                        {range}
+                                    </NavDropdown.Item>
 
+                                    <div className={'submenu'} id={`submenu-md-${range}`}>
+                                        {equals(range, 'single') && Object.keys(cache.entities).map((entity) => (
+                                            <NavDropdown.Item key={entity} href={`/edit/${entity}?uuid=register`}>
+                                                {_t(entity)}
+                                            </NavDropdown.Item>
+                                        ))}
+
+                                        {equals(range, 'bulk') && supportedBulkRegister().map((entity) => (
+                                            <NavDropdown.Item key={entity} href={`/edit/bulk/${entity}?action=register`}>
+                                                {entity}s
+                                            </NavDropdown.Item>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
                         </NavDropdown>
+
                         <NavDropdown
                             active={false}
                             variant={'primary'}
                             hidden={hidden}
-                            title="Bulk register entities"
-                            id="nav-dropdown--bulkCreate">
-                            {supportedBulkRegister().map((entity) => (
-                                <NavDropdown.Item key={entity} href={`/edit/bulk/${entity}?action=register`}>
-                                    {entity}s
-                                </NavDropdown.Item>
-                            ))}
-
-                        </NavDropdown>
-                        <NavDropdown
-                            active={false}
-                            variant={'primary'}
-                            hidden={hidden}
-                            title="Bulk upload metadata"
+                            title="Upload metadata"
                             id="nav-dropdown--bulkMetadata">
                             {Object.keys(supportedMetadata()).map((entity, key) => (
                                 <div key={`dropdownItem-md-${entity}`}>
@@ -96,12 +102,17 @@ const AppNavbar = ({hidden, signoutHidden}) => {
                                                               className={'is-subItem'}>
                                                 <span>{type[1]}</span>
                                             </NavDropdown.Item>
-
                                         ))}
                                     </div>
                                 </div>
                             ))}
                         </NavDropdown>
+                        <Nav.Link href={equals(window.location.pathname, APP_ROUTES.searchFiles) ? APP_ROUTES.search : APP_ROUTES.searchFiles}>
+                            {equals(window.location.pathname, APP_ROUTES.searchFiles) ? _t('Entity search') : _t('File search')}
+                        </Nav.Link>
+                        <Nav.Link href={'https://docs.sennetconsortium.org/'}>
+                            {_t('Documentation')}
+                        </Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link
