f70ca450f3ccadff927ab4b5554fb8b15a3ad2c3
maxsibilla <maxsibilla@icloud.com>
2023-08-09 11:15:03 -0400

    Updating docker to remove nginx and use next.js server for deployment. Reverting disabling image optimization to test if this setup also prevents issue with images loading

--- docker/portal-ui/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start nginx in background
-# 'daemon off;' is nginx configuration directive
-nginx -g 'daemon off;' &
-
-# Start Node.js server and keep it running in foreground
-npm run start
--- src/next.config.js
+++ src/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-    },
+    // images: {
+    //     unoptimized: true,
+    // },
     reactStrictMode: false, // Don't render components twice
 }
 
