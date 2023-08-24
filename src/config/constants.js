a5fc02dbec0cbbe01d8950331ba58a1d9a9d88ac
Lisa-Ann B <lisabruney@pitt.edu>
2023-08-24 13:40:26 -0400

    Resolve issue with redirect on search/files - #810

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,6 @@
 export const APP_ROUTES = {
     "home": "/",
     "search": "/search",
-    "searchFiles": "/search/files",
     "login": "/login",
     "logout": "/logout",
     "notFound": "/404"
