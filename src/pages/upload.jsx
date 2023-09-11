fcdbb75f37129f5960ea94c85b6dcaf857e6afc8
Lisa-Ann B <lisabruney@pitt.edu>
2023-09-11 14:58:58 -0400

    Add upload view page - #852

---
+++ src/pages/upload.jsx
@@ -0,0 +1,142 @@
+import React, {Fragment, useContext, useEffect, useState} from "react";
+import {useRouter} from 'next/router';
+import Description from "../components/custom/entities/sample/Description";
+import MetadataTable from "../components/custom/entities/MetadataTable";
+import Attribution from "../components/custom/entities/sample/Attribution";
+import log from "loglevel";
+import {equals, getRequestHeaders} from "../components/custom/js/functions";
+import AppNavbar from "../components/custom/layout/AppNavbar";
+import {get_write_privilege_for_group_uuid} from "../lib/services";
+import Unauthorized from "../components/custom/layout/Unauthorized";
+import Protocols from "../components/custom/entities/sample/Protocols";
+import AppFooter from "../components/custom/layout/AppFooter";
+import Header from "../components/custom/layout/Header";
+import Spinner from "../components/custom/Spinner";
+import AppContext from "../context/AppContext";
+import Alert from 'react-bootstrap/Alert';
+import Provenance from "../components/custom/entities/Provenance";
+import {EntityViewHeader} from "../components/custom/layout/entity/ViewHeader";
+import SidebarBtn from "../components/SidebarBtn";
+import Metadata from "../components/custom/entities/Metadata";
+
+function ViewUpload() {
+    const router = useRouter()
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [hasWritePrivilege, setHasWritePrivilege] = useState(false)
+    const {isRegisterHidden, isLoggedIn, isUnauthorized, isAuthorizing, _t, cache} = useContext(AppContext);
+
+    // only executed on init rendering, see the []
+    useEffect(() => {
+        // declare the async data fetching function
+        const fetchData = async (uuid) => {
+
+            log.debug('upload: getting data...', uuid)
+            // get the data from the api
+            const response = await fetch("/api/find?uuid=" + uuid, getRequestHeaders());
+            // convert the data to json
+            const data = await response.json();
+
+            log.debug('upload: Got data', data)
+            if (data.hasOwnProperty("error")) {
+                setError(true)
+                setErrorMessage(data["error"])
+                setData(false)
+            } else {
+                // set state with the result
+                setData(data);
+                get_write_privilege_for_group_uuid(data.group_uuid).then(response => {
+                    setHasWritePrivilege(response.has_write_privs)
+                }).catch(log.error)
+            }
+        }
+
+        if (router.query.hasOwnProperty("uuid")) {
+            // call the function
+            fetchData(router.query.uuid)
+                // make sure to catch any error
+                .catch(console.error);
+            ;
+        } else {
+            setData(null);
+        }
+    }, [router]);
+
+    console.log("Test cache in source: ", cache)
+
+    if ((isAuthorizing() || isUnauthorized()) && !data) {
+        return (
+            data == null ? <Spinner/> : <Unauthorized/>
+        )
+    } else {
+        return (
+            <>
+                {data && <Header title={`${data.sennet_id} | Source | SenNet`}></Header>}
+
+                <AppNavbar hidden={isRegisterHidden} signoutHidden={false}/>
+
+                {error &&
+                    <div><Alert variant='warning'>{_t(errorMessage)}</Alert></div>
+                }
+                {data && !error &&
+                    <>
+                        <div className="container-fluid">
+                            <div className="row flex-nowrap entity_body">
+                                <div className="col-auto p-0">
+                                    <div id="sidebar"
+                                         className="collapse collapse-horizontal sticky-top custom-sticky">
+                                        <ul id="sidebar-nav"
+                                            className="nav list-group rounded-0 text-sm-start">
+                                            <li className="nav-item">
+                                                <a href="#Summary"
+                                                   className="nav-link "
+                                                   data-bs-parent="#sidebar">Summary</a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <a href="#Files"
+                                                   className="nav-link"
+                                                   data-bs-parent="#sidebar">Files</a>
+                                            </li>
+                                            <li className="nav-item">
+                                                <a href="#Datasets"
+                                                   className="nav-link"
+                                                   data-bs-parent="#sidebar">Datasets</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <main className="col m-md-3 entity_details">
+                                    <SidebarBtn/>
+
+                                    <EntityViewHeader data={data} entity={cache.entities.upload.toLowerCase()}
+                                                      hasWritePrivilege={hasWritePrivilege}/>
+
+                                    <div className="row">
+                                        <div className="col-12">
+                                            {/*Description*/}
+                                            <Description primaryDateTitle="Creation Date"
+                                                         primaryDate={data.created_timestamp}
+                                                         secondaryDateTitle="Modification Date"
+                                                         secondaryDate={data.last_modified_timestamp}
+                                                         labId={data.title}
+                                            />
+
+
+                                        </div>
+                                    </div>
+                                </main>
+                            </div>
+                        </div>
+                    </>
+                }
+                <AppFooter/>
+            </>
+        )
+    }
+
+}
+
+
+export default ViewUpload
