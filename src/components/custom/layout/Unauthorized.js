9445ae6b038ee17f1c9c66ec198898ba06118326
maxsibilla <maxsibilla@icloud.com>
2023-10-10 09:07:46 -0400

    Updating unauthorized message for non-logged in users

--- src/components/custom/layout/Unauthorized.js
+++ src/components/custom/layout/Unauthorized.js
@@ -9,8 +9,18 @@ const Unauthorized = () => {
     return (
         <Container className={'mt-5'}>
             <Row>
-                <Col md={{span: 8, offset:2}}>
-                    {!email && <div className={'alert alert-danger text-center'}>Access denied</div>}
+                <Col md={{span: 8, offset: 2}}>
+                    {!email && <div className={'alert alert-danger'}>
+                        <p>
+                            You are trying to access the Data Portal without being signed in.
+                            You must be authorized in order to access this page. Please <a href={APP_ROUTES.login}>log
+                            in</a> then attempt to visit this page again.
+                        </p>
+                        <p>
+                            If you continue to have issues accessing this site please contact the <a
+                            href={"mailto:help@sennetconsortium.org"}>SenNet Help Desk</a>.
+                        </p>
+                    </div>}
 
                     {email && <div className={'alert alert-danger'}>
                         <p>
@@ -21,7 +31,8 @@ const Unauthorized = () => {
                             information and are logging in with that account.
                         </p>
                         <p>
-                            Once you have confirmed your registration information you can <a href={APP_ROUTES.login}>log in</a> again.
+                            Once you have confirmed your registration information you can <a href={APP_ROUTES.login}>log
+                            in</a> again.
                             Without login you can view the public facing <a href={APP_ROUTES.search}>Data Portal</a>.
                         </p>
                         <p>
