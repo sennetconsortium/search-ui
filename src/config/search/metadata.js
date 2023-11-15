2f1630b288fc5c7437f905a27b7f073855126f66
Tyler Madonna <tjm159@pitt.edu>
2023-11-15 09:59:38 -0500

    Removed protein dataset from the metadata discover

--- src/config/search/metadata.js
+++ src/config/search/metadata.js
@@ -478,11 +478,5 @@ export const SEARCH_METADATA = {
             entityType: 'dataset',
             queryString: 'size=n_10000_n&filters%5B0%5D%5Bfield%5D=entity_type&filters%5B0%5D%5Bvalues%5D%5B0%5D=Dataset&filters%5B0%5D%5Btype%5D=any&filters%5B1%5D%5Bfield%5D=metadata.metadata.analyte_class&filters%5B1%5D%5Bvalues%5D%5B0%5D=RNA&filters%5B1%5D%5Btype%5D=any&sort%5B0%5D%5Bfield%5D=last_modified_timestamp&sort%5B0%5D%5Bdirection%5D=desc'
         },
-        {
-            title: 'All Protein Datasets',
-            description: 'Datasets with the Protein analyte class.',
-            entityType: 'dataset',
-            queryString: 'size=n_10000_n&filters%5B0%5D%5Bfield%5D=entity_type&filters%5B0%5D%5Bvalues%5D%5B0%5D=Dataset&filters%5B0%5D%5Btype%5D=any&filters%5B1%5D%5Bfield%5D=metadata.metadata.analyte_class&filters%5B1%5D%5Bvalues%5D%5B0%5D=protein&filters%5B1%5D%5Btype%5D=any&sort%5B0%5D%5Bfield%5D=last_modified_timestamp&sort%5B0%5D%5Bdirection%5D=desc'
-        },
     ],
 }
