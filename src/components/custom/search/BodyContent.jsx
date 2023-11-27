41b453b1a60df934c3285de93a520273c39dabcd
Lisa-Ann B <lisabruney@pitt.edu>
2023-11-27 11:00:47 -0500

    Remove unnecessary Spinner in BodyContent - #1048

--- src/components/custom/search/BodyContent.jsx
+++ src/components/custom/search/BodyContent.jsx
@@ -13,7 +13,6 @@ function BodyContent({ view }) {
             data-ada-data='{"trigger": ".rdt_TableCell", "tabIndex": ".rdt_TableRow"}'
         >
             {wasSearched && <Results filters={filters} titleField={filters} rawResponse={rawResponse} view={view} />}
-            {!wasSearched && <Spinner />}
         </div>
     )
 }
