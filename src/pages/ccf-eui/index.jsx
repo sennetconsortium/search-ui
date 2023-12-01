3f0b11cde6fc8c5e50ed7010497dafcf9ebc0f15
maxsibilla <maxsibilla@icloud.com>
2023-12-01 11:52:16 -0500

    Modify imports for the RUI and EUI tools. Modifying the EUIIntegration to specify base href and remove navbar

--- src/pages/ccf-eui/index.jsx
+++ src/pages/ccf-eui/index.jsx
@@ -1,33 +1,9 @@
-import { useEffect, useRef, useState } from "react";
-
-import AppNavbar from "../../components/custom/layout/AppNavbar";
 import EUIIntegration from "../../components/custom/eui/EUIIntegration";
 
 const EUIExploration = () => {
-    const navBarRef = useRef(null);
-    const [euiHeight, setEUIHeight] = useState("50vh");
-
-    useEffect(() => {
-        resizeEUI();
-        window.addEventListener("resize", resizeEUI);
-
-        return () => {
-            window.removeEventListener("resize", resizeEUI);
-        };
-    }, []);
-
-    function resizeEUI() {
-        const navBar = navBarRef.current;
-        if (!navBar) return;
-        const euiHeight = `${window.innerHeight - navBar.offsetHeight - 10}px`;
-        setEUIHeight(euiHeight);
-    }
 
     return (
-        <div className="m-0 h-100 overflow-hidden">
-            <AppNavbar innerRef={navBarRef} />
-            <EUIIntegration height={euiHeight} />
-        </div>
+        <EUIIntegration/>
     );
 };
 
