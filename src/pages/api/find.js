bd1184ff847bdb44c671ab17f514590f04dbfafe
Lisa-Ann B <lisabruney@pitt.edu>
2023-12-06 12:01:55 -0500

    Change condition for test

--- src/pages/api/find.js
+++ src/pages/api/find.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
                         res.status(401).json(result)
                     } else {
                         var total = result["hits"]["total"]["value"]
-                        if (total === 1) {
+                        if (total !== 0) {
                             var entity = result["hits"]["hits"][0]["_source"]
                             res.status(200).json(entity)
                         } else {
