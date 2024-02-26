1c8b8c0d53fde576e0d70cf7e61528e563a64093
Lisa-Ann B <lisabruney@pitt.edu>
2024-02-26 16:01:20 -0500

    Add independent search bar functionality for RDT - #1231

---
+++ src/hooks/useDataTableSearch.js
@@ -0,0 +1,54 @@
+import React, {useContext, useEffect, useState, useMemo} from 'react'
+import PropTypes from 'prop-types'
+
+import {Button, Form, Row, Container, Col} from 'react-bootstrap'
+
+const FilterComponent = ({ filterText, onFilter, onClear, fieldsToSearch }) => (
+    <>
+        <Form.Control defaultValue={filterText} onChange={onFilter}/>
+        <Button type="button" onClick={onClear}>X</Button>
+    </>
+);
+
+function useDataTableSearch(data,  idField, fieldsToSearch = []) {
+    useEffect(() => {
+    }, [])
+
+    const [filterText, setFilterText] = useState('');
+    const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
+    const filteredItems = () => {
+        let results = []
+        let dict = {};
+        for (let searchIndex of fieldsToSearch) {
+            for (let d of data) {
+                if (!dict[d[idField]] || !idField) {
+                    if (d[searchIndex] && d[searchIndex].toLowerCase().includes(filterText.toLowerCase())) {
+                        results.push(d);
+                        if (idField) {
+                            dict[d[idField]] = true;
+                        }
+
+                    }
+                }
+            }
+        }
+        return results;
+    }
+    const searchBarComponent = useMemo(() => {
+        const handleClear = () => {
+            if (filterText) {
+                setResetPaginationToggle(!resetPaginationToggle);
+                setFilterText('');
+            }
+        };
+
+        return (
+            <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} fieldsToSearch={fieldsToSearch} />
+        );
+    }, [filterText, resetPaginationToggle]);
+
+    return {filteredItems: filteredItems(), filterText, searchBarComponent}
+}
+
+
+export default useDataTableSearch
