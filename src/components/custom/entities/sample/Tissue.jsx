6ebb8632c9a5fac39c0e7caf1b62bd0a15208b20
Lisa-Ann B <lisabruney@pitt.edu>
2024-03-01 13:30:08 -0500

    Update old Tissue component - #1259

--- src/components/custom/entities/sample/Tissue.jsx
+++ src/components/custom/entities/sample/Tissue.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
-import {Table} from 'react-bootstrap';
+import {Table, Card} from 'react-bootstrap';
+import SenNetAccordion from "../../layout/SenNetAccordion";
+import {displayBodyHeader, getUBKGFullName} from "../../js/functions";
+import DataTable from "react-data-table-component";
 
-export default class Tissue extends React.Component {
-    render() {
+export default function Tissue({ data }) {
+
+    const columns = [
+        {
+            name: 'Organ',
+            selector: row => getUBKGFullName(row.origin_sample?.organ),
+            width: '120px',
+            format: row => <a href={`/organs/${getUBKGFullName(row.origin_sample?.organ).toLowerCase()}`}>{getUBKGFullName(row.origin_sample?.organ)}</a>,
+        },
+        {
+            name: 'Category',
+            selector: row => row.sample_category,
+            width: '200px',
+            format: row => {
+                return <span>{row.sample_category ? displayBodyHeader(row.sample_category) : ''}</span>
+            }
+        },
+        {
+            name: 'Tissue Location',
+            selector: row => row.rui_location,
+            omit: data.rui_location.length <= 0,
+            format: row => {
+                return <div>The <a href={`/api/json?view=${btoa(row.rui_location)}`} target={'_blank'}>spatial coordinates of this sample</a> have been registered and it can be found in the <a target={'_blank'} href={'/ccf-eui'}>Common Coordinate Framework Exploration User Interface</a>.</div>
+            }
+        }
+    ]
+
         return (
-            <li className="sui-result" id="Tissue">
-                <div className="sui-result__header">
-                    <span className="sui-result__title">Tissue</span>
-                </div>
-                <div className="card-body">
-                    <Table borderless>
-                        <thead>
-                        <tr>
-                            <th>Organ Type</th>
-                            <th>Sample Category</th>
-                            {this.props.data.rui_location &&
-                                <th>Tissue Location</th>
-                            }
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>
-                                {/*Have to do this due to how react processes embedded objects*/}
-                                {this.props.data.organ &&
-                                    <span>
-                                        {this.props.data.organ}
-                                    </span>
-                                }
-                            </td>
-                            <td>{this.props.data.sample_category}</td>
-                            {this.props.data.rui_location &&
-                                <td>{this.props.data.rui_location}</td>
-                            }
-                        </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </li>
+            <SenNetAccordion title={'Tissue'}>
+                <Card border='0' className='mb-2 pb-2'>
+                    <DataTable columns={columns} data={[data]} />
+                </Card>
+            </SenNetAccordion>
         )
-    }
+
 }
