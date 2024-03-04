6d237f688663da0ce64faa41f8ca2109f5975a37
maxsibilla <maxsibilla@icloud.com>
2024-03-04 13:06:16 -0500

    Setting processed dataset by reverse walking prov list

--- src/context/DerivedContext.jsx
+++ src/context/DerivedContext.jsx
@@ -51,8 +51,8 @@ export const DerivedProvider = ({children}) => {
                 if (prov_info !== {}) {
                     const processed_datasets = prov_info['processed_dataset_uuid']
                     const processed_dataset_statuses = prov_info['processed_dataset_status']
-                    // Iterate over processed datasets and check that the status is valid
-                    for (let i = 0; i < processed_dataset_statuses.length; i++) {
+                    // Reverse iterate over processed datasets and check that the status is valid
+                    for (let i = processed_dataset_statuses.length-1; i > 0; i--) {
                         if (isDatasetStatusPassed(processed_dataset_statuses[i])) {
                             fetchEntity(processed_datasets[i]).then(processed_dataset => {
                                 // Check that the assay type is supported by Vitessce
