96965530bb02495532f1421504899e95cfd2eca4
maxsibilla <maxsibilla@icloud.com>
2024-03-05 08:50:56 -0500

    Updating vitessce to latest version

--- src/components/custom/vitessce/SennetVitessce.js
+++ src/components/custom/vitessce/SennetVitessce.js
@@ -39,7 +39,7 @@ export const SennetVitessce = ({data}) => {
                         <span className={'fw-light fs-6'}>Powered by
                             <a className={'ms-2'} target="_blank" href="http://vitessce.io/" rel="noopener noreferrer"
                                title={'Vitessce.io'}>
-                                Vitessce V3.3.4
+                                Vitessce V3.3.7
                             </a>
                         </span>
                     </div>
