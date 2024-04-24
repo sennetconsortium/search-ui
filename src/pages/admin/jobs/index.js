3b6f6f505aee2d2b280546db274ecffb82ab3c03
Lisa-Ann B <lisabruney@pitt.edu>
2024-04-24 09:17:55 -0400

    Add logic to handle not logged in or unauthorized login for admin/jobs - #1389

--- src/pages/admin/jobs/index.js
+++ src/pages/admin/jobs/index.js
@@ -1,17 +1,26 @@
 import ViewJobs from "../../user/jobs";
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import AppContext from "../../../context/AppContext";
 import Spinner from "../../../components/custom/Spinner";
 import {JobQueueProvider} from "../../../context/JobQueueContext";
+import {APP_ROUTES} from "../../../config/constants";
+import Unauthorized from "../../../components/custom/layout/Unauthorized";
 
 
 function ViewJobsAdmin() {
-    const {adminGroup} = useContext(AppContext)
+    const {adminGroup, isLoggedIn, router} = useContext(AppContext)
+
+    useEffect(() => {
+        if (!isLoggedIn()) {
+            router.push(APP_ROUTES.login)
+            return
+        }
+    }, [])
 
     return (
         <>
             {adminGroup && <ViewJobs isAdmin={adminGroup} />}
-            {!adminGroup && <Spinner/>}
+            {isLoggedIn() && !adminGroup && <Unauthorized />}
         </>
     )
 }
