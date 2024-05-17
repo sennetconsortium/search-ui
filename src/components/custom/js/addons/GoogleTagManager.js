6909faba4940a3788de38e5988d24ef90b7ba374
Lisa-Ann B <lisabruney@pitt.edu>
2024-05-17 10:26:25 -0400

    Transform from ES6 class to regular object - #1416

--- src/components/custom/js/addons/GoogleTagManager.js
+++ src/components/custom/js/addons/GoogleTagManager.js
@@ -1,173 +1,175 @@
 import Addon from './Addon'
 import $ from 'jquery'
 
-class GoogleTagManager extends Addon {
-    constructor(el, args) {
-        super(el, args)
-        this.sel = {
+let GTM = {
+    constructor: (el, args, _t) => {
+        GTM = Object.assign(GTM, _t)
+        GTM.main(el, args, GTM)
+
+        GTM.sel = {
             facets: {
                 title: '.sui-facet__title'
             }
         }
-        this.extractEvent()
-        this.modules()
-    }
+        GTM.extractEvent()
+        GTM.modules()
+    },
 
-    extractEvent() {
-        if (this.el == null || !this.el.length) return
-        this.el[0].classList.forEach(((val) => {
-            if (val.indexOf(this.app) !== -1) {
-                this.event = val.split('--')[1]
+    extractEvent: () => {
+        if (GTM.el == null || !GTM.el.length) return
+        GTM.el[0].classList.forEach((val) => {
+            if (val.indexOf(GTM.app) !== -1) {
+                GTM.event = val.split('--')[1]
             }
-        }).bind(this))
-        this.log(`Google Tag manager ... ${this.event}`)
-    }
+        })
+        GTM.log(`Google Tag manager ... ${GTM.event}`)
+    },
 
-    modules() {
-        switch (this.event) {
+    modules: () => {
+        switch (GTM.event) {
             case 'search':
-                this.search()
+                GTM.search()
                 break
             case 'facets':
-                this.facets()
+                GTM.facets()
                 break
             case 'dateFacets':
-                this.dateFacets()
+                GTM.dateFacets()
                 break
             case 'results':
-                this.results()
+                GTM.results()
                 break
             default:
-                this.numericFacets() // As these facets can be conditional
-                this.page()
-                this.cta()
-                this.links()
+                GTM.numericFacets() // As these facets can be conditional
+                GTM.page()
+                GTM.cta()
+                GTM.links()
         }
-    }
+    },
 
-    handleSearch(e) {
-        const keywords = this.currentTarget(e).parent().find('#search').val()
-        this.gtm({ keywords })
-    }
+    handleSearch: (e) => {
+        const keywords = GTM.currentTarget(e).parent().find('#search').val()
+        GTM.gtm({ keywords })
+    },
 
-    search() {
-        this.el.on('click', 'button', ((e) => {
-            this.handleSearch(e)
-        }).bind(this))
+    search: () => {
+        GTM.el.on('click', 'button', (e) => {
+            GTM.handleSearch(e)
+        })
 
-        this.el.on('keydown', 'button, input', ((e) => {
-            if (this.isEnter(e)) this.handleSearch(e)
-        }).bind(this))
-    }
+        GTM.el.on('keydown', 'button, input', (e) => {
+            if (GTM.isEnter(GTM, e)) GTM.handleSearch(GTM, e)
+        })
+    },
 
-    handleFacets(e) {
+    handleFacets: (e) => {
         const label = $(e.target).parent().find('.sui-multi-checkbox-facet__input-text').text()
-        this.gtm({ group: this.group, label, trail: `${this.group}.${label}` })
-    }
+        GTM.gtm({ group: GTM.group, label, trail: `${GTM.group}.${label}` })
+    },
 
-    handleDateFacets(e) {
+    handleDateFacets: (e) => {
         // const label = this.currentTarget(e).val()
-        this.event = 'facets'
-        this.gtm({ group: this.group, label: this.subGroup, trail: `${this.group}.${this.subGroup}` })
-    }
+        GTM.event = 'facets'
+        GTM.gtm({ group: GTM.group, label: GTM.subGroup, trail: `${GTM.group}.${GTM.subGroup}` })
+    },
 
-    handleNumericFacets(e) {
-        this.event = 'facets'
-        let label = this.currentTarget(e).find('input').val()
-        this.gtm({ group: this.group, label, trail: `${this.group}.${label}` })
-    }
+    handleNumericFacets: (e) => {
+        GTM.event = 'facets'
+        let label = GTM.currentTarget(e).find('input').val()
+        GTM.gtm({ group: GTM.group, label, trail: `${GTM.group}.${label}` })
+    },
 
-    storeLoaded(key) {
+    storeLoaded: (key) => {
         const $body = $('body')
         if ($body.data(key)) return null
         $body.attr(`data-${key}`, true)
         return `.js-gtm--${key}`
-    }
+    },
 
-    facets() {
-        const sel = this.storeLoaded('facets')
+    facets: () => {
+        const sel = GTM.storeLoaded('facets')
         if (!sel) return
         const pre = 'sui-multi-checkbox-facet__'
         const body = document.querySelector('body')
-        body.addEventListener('click', ((e) => {
+        body.addEventListener('click', (e) => {
             const el = e.target
             if (el.classList.contains(`${pre}checkbox`) || el.classList.contains(`${pre}option-input-wrapper`)) {
-                this.group = $(el).parents(sel).parent().find(this.sel.facets.title).text()
-                this.handleFacets(e)
+                GTM.group = $(el).parents(sel).parent().find(GTM.sel.facets.title).text()
+                GTM.handleFacets(e)
             }
-        }).bind(this),true)
-    }
+        },true)
+    },
 
-    dateFacets() {
-        this.group = this.el.parent().find(this.sel.facets.title).text()
-        this.subGroup = this.el.find('.sui-multi-checkbox-facet').text()
-        this.el.on(
+    dateFacets: () => {
+        GTM.group = GTM.el.parent().find(GTM.sel.facets.title).text()
+        GTM.subGroup = GTM.el.find('.sui-multi-checkbox-facet').text()
+        GTM.el.on(
             'change',
-            'input',
-            ((e) => {
-                this.stop(e)
-                this.handleDateFacets(e)
-            }).bind(this)
+            'input', (e) => {
+                GTM.stop(e)
+                GTM.handleDateFacets(e)
+            }
         )
-    }
+    },
 
-    numericFacets() {
-        const sel = this.storeLoaded('numericFacets')
+    numericFacets: () => {
+        const sel = GTM.storeLoaded('numericFacets')
         if (!sel) return
         $('body').on(
             'DOMSubtreeModified',
             `${sel} .MuiSlider-thumb`,
-            ((e) => {
-                this.stop(e)
-                const $el = this.currentTarget(e)
+            (e) => {
+                GTM.stop(e)
+                const $el = GTM.currentTarget(e)
                 let val = $el.attr('data-val')
                 let ioVal = $el.find('input').val()
                 // on DOMSubtreeModified, multiple triggers for same value, so only gtm once
                 if (val !== ioVal) {
                     $el.attr('data-val', ioVal)
-                    this.group = this.currentTarget(e).parents(sel).parent().parent().find(this.sel.facets.title).text()
-                    this.handleNumericFacets(e)
+                    GTM.group = GTM.currentTarget(e).parents(sel).parent().parent().find(GTM.sel.facets.title).text()
+                    GTM.handleNumericFacets(e)
                 }
-            }).bind(this)
+            }
         )
-    }
+    },
 
-    handleResults(e) {
-        const td = this.currentTarget(e)
+    handleResults: (e) => {
+        const td = GTM.currentTarget(e)
         const th = ['sennet_id', 'entity_type', 'lab_id', 'group_name']
         const data = {}
         for (let i = 0; i < th.length; i++) {
             data[th[i]] = td.parent().find(`[data-field="${th[i]}"]`).text().trim()
         }
-        this.gtm(data)
-    }
+        GTM.gtm(data)
+    },
 
-    results() {
-        this.el.on('click', '.rdt_TableBody .rdt_TableCell:not(div[data-column-id="1"])', ((e) => {
-            this.handleResults(e)
-        }).bind(this))
-    }
+    results: () => {
+        GTM.el.on('click', '.rdt_TableBody .rdt_TableCell:not(div[data-column-id="1"])', (e) => {
+            GTM.handleResults(e)
+        })
+    },
 
-    getPath() {
+    getPath: () => {
         const path = window.location.pathname + window.location.search
         return path.length > 70 ? window.location.pathname : path;
-    }
-    handleLinks(e) {
-        this.event = 'links'
-        const $el = this.currentTarget(e)
-        this.gtm({link: $el.text() || $el.attr('aria-label') || $el.attr('alt')})
-    }
+    },
 
-    links() {
-        $('a').on('click', ((e) => {
-            this.handleLinks(e)
-        }).bind(this))
-    }
+    handleLinks: (e) => {
+        GTM.event = 'links'
+        const $el = GTM.currentTarget(e)
+        GTM.gtm({link: $el.text() || $el.attr('aria-label') || $el.attr('alt')})
+    },
 
-    handleCta(e) {
-        const $el = this.currentTarget(e)
+    links: () => {
+        $('a').on('click', (e) => {
+            GTM.handleLinks(e)
+        })
+    },
+
+    handleCta: (e) => {
+        const $el = GTM.currentTarget(e)
         const className = $el.attr('class')
-        this.event = 'cta'
+        GTM.event = 'cta'
         let action
         const actions = ['json', 'submit', 'login', 'save', 'revert', 'validate', 'reorganize', 'process']
         if (className) {
@@ -180,44 +182,44 @@ class GoogleTagManager extends Addon {
 
         if (action) {
             let data = { }
-            data = this.entityPage(data, false)
+            data = GTM.entityPage(data, false)
             let action2 = data.action || null
-            data = { ...data, action: (action2 ? `${action}.${action2}`: action), uuid: this.getUuid() }
-            this.gtm(data)
+            data = { ...data, action: (action2 ? `${action}.${action2}`: action), uuid: GTM.getUuid() }
+            GTM.gtm(data)
         }
 
-    }
+    },
 
-    cta() {
-        $('body').on('click', '[role="button"], .btn, button', ((e) => {
-            this.handleCta(e)
-        }).bind(this))
-    }
+    cta: () => {
+        $('body').on('click', '[role="button"], .btn, button', (e) => {
+            GTM.handleCta(e)
+        })
+    },
 
-    page() {
-        this.event = 'page'
+    page: () => {
+        GTM.event = 'page'
         let data = {data: 'view'}
         Addon.log('GTM, Page event ...', 'log', 'pink')
-        this.entityPage(data)
-    }
+        GTM.entityPage(data)
+    },
 
-    getUuid() {
-        const uuid = this.router.asPath.split('uuid=')
+    getUuid: () => {
+        const uuid = GTM.router.asPath.split('uuid=')
         // Fully fetch the uuid, split(&)[0] in case more params follow
-        return uuid.length && uuid.length > 1 ? this.router.asPath.split('uuid=')[1].split('&')[0] : null
-    }
+        return uuid.length && uuid.length > 1 ? GTM.router.asPath.split('uuid=')[1].split('&')[0] : null
+    },
 
-    entityPage(data, send = true) {
+    entityPage: (data, send = true) => {
 
-        const entities = Object.keys(this.entities)
+        const entities = Object.keys(GTM.entities)
 
         let pos = -1
         for (let i = 0; i < entities.length; i++) {
-            if (this.router.route.includes(entities[i])) pos = i
+            if (GTM.router.route.includes(entities[i])) pos = i
         }
 
         if ( pos !== -1) {
-            data.entity = this.entities[entities[pos]]
+            data.entity = GTM.entities[entities[pos]]
             const actions = ['register', 'edit']
             for (let action of actions) {
                 if (window.location.href.indexOf(action) !== -1) {
@@ -225,38 +227,40 @@ class GoogleTagManager extends Addon {
                     break
                 }
             }
-            data.uuid = this.getUuid()
+            data.uuid = GTM.getUuid()
 
             if (send) {
-                this.gtm(data) // push Page view
-                this.event = 'entity'
-                this.gtm(data) // push Entity Page view
+                GTM.gtm(data) // push Page view
+                GTM.event = 'entity'
+                GTM.gtm(data) // push Entity Page view
             }
         } else {
-            if (send) this.gtm(data) // push Page view
+            if (send) GTM.gtm(data) // push Page view
         }
         return data
-    }
-    getPerson(bto = false) {
-        const id = this.user.email
+    },
+
+    getPerson: (bto = false) => {
+        const id = GTM.user.email
         let result
         if (id) {
             result = bto ? btoa(id.replace('@', '*')) : `${id.split('@')[0]}***`
         }
         return result
-    }
-    gtm(args) {
+    },
+
+    gtm: (args) => {
         let data = {
-            event: this.event,
-            path: this.getPath(),
-            person: this.getPerson(),
-            user_id: this.getPerson(true),
-            globus_id: this.user.globus_id,
-            session: (this.user.email !== undefined), ...args
+            event: GTM.event,
+            path: GTM.getPath(),
+            person: GTM.getPerson(),
+            user_id: GTM.getPerson(true),
+            globus_id: GTM.user.globus_id,
+            session: (GTM.user.email !== undefined), ...args
         }
-        if (Addon.isLocal()) console.log(data)
+        if (GTM.isLocal()) console.log(data)
         dataLayer.push(data)
     }
 }
 
-export default GoogleTagManager
+export default GTM
