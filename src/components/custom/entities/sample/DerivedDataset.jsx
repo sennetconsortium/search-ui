08882361c751a7f47dc8a781e13ea33363ab9efe
Lisa-Ann B <lisabruney@pitt.edu>
2024-05-20 12:31:48 -0400

    Convert DerivedDataset (not used)

--- src/components/custom/entities/sample/DerivedDataset.jsx
+++ src/components/custom/entities/sample/DerivedDataset.jsx
@@ -2,77 +2,47 @@ import React from 'react';
 import {Container, Tab, Table, Tabs} from 'react-bootstrap';
 import DerivedDatasetItem from "./DerivedDatasetItem";
 
-export default class DerivedDataset extends React.Component {
-    render() {
-        return (
-            <li className="sui-result" id="Derived-Datasets">
-                <div className="sui-result__header">
-                    <span
-                        className="sui-result__title">{this.props.includeSample ? "Derived" : "Derived Datasets"}</span>
-                    <div>
-                        {this.props.includeSample &&
-                            <span>{this.props.data.descendant_counts.entity_type.Sample} Sample(s) | </span>
-                        }
-                        <span>{this.props.data.descendant_counts.entity_type.Dataset} Datset(s)</span>
-                    </div>
-                </div>
-                <div className="card-body">
-                    <Tabs
-                        defaultActiveKey={this.props.includeSample ? "samples" : "datasets"}
-                        transition={false}
-                        id="datasets-tab"
-                    >
-                        {/*Samples section specifically for Source*/}
-                        {this.props.includeSample &&
-                            <Tab eventKey="samples" title="Samples">
-                                <Container className={`mt-3 table--derivedDataset`}>
-                                    <Table>
-                                        <thead>
-                                        <tr>
-                                            <th>SenNet ID</th>
-                                            <th>Organ</th>
-                                            <th>Sample Category</th>
-                                            <th>Derived Dataset Count</th>
-                                            <th>Last Modified</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        {this.props.data.descendants.map((descendant_data, index) => {
-                                            if (descendant_data.entity_type === 'Sample') {
-                                                return (
-                                                    <React.Fragment key={index}>
-                                                        <DerivedDatasetItem index={index} data={this.props.data}
-                                                                            data_type="Sample"
-                                                                            descendant_uuid={descendant_data.uuid}/>
-                                                    </React.Fragment>
-                                                )
-                                            }
-                                        })}
-                                        </tbody>
-                                    </Table>
-                                </Container>
-                            </Tab>
-                        }
+const DerivedDataset = ({includeSample, data}) => {
 
-                        <Tab eventKey="datasets" title="Datasets">
+    return (
+        <li className="sui-result" id="Derived-Datasets">
+            <div className="sui-result__header">
+                <span
+                    className="sui-result__title">{includeSample ? "Derived" : "Derived Datasets"}</span>
+                <div>
+                    {includeSample &&
+                        <span>{data.descendant_counts.entity_type.Sample} Sample(s) | </span>
+                    }
+                    <span>{data.descendant_counts.entity_type.Dataset} Dataset(s)</span>
+                </div>
+            </div>
+            <div className="card-body">
+                <Tabs
+                    defaultActiveKey={includeSample ? "samples" : "datasets"}
+                    transition={false}
+                    id="datasets-tab"
+                >
+                    {/*Samples section specifically for Source*/}
+                    {includeSample &&
+                        <Tab eventKey="samples" title="Samples">
                             <Container className={`mt-3 table--derivedDataset`}>
                                 <Table>
                                     <thead>
                                     <tr>
                                         <th>SenNet ID</th>
-                                        <th>Data Types</th>
-                                        <th>Status</th>
+                                        <th>Organ</th>
+                                        <th>Sample Category</th>
                                         <th>Derived Dataset Count</th>
                                         <th>Last Modified</th>
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    {this.props.data.descendants.map((descendant_data, index) => {
-                                        if (descendant_data.entity_type === 'Dataset') {
+                                    {data.descendants.map((descendant_data, index) => {
+                                        if (descendant_data.entity_type === 'Sample') {
                                             return (
                                                 <React.Fragment key={index}>
-                                                    <DerivedDatasetItem index={index} data={this.props.data}
-                                                                        data_type="Dataset"
+                                                    <DerivedDatasetItem index={index} data={data}
+                                                                        data_type="Sample"
                                                                         descendant_uuid={descendant_data.uuid}/>
                                                 </React.Fragment>
                                             )
@@ -82,9 +52,40 @@ export default class DerivedDataset extends React.Component {
                                 </Table>
                             </Container>
                         </Tab>
-                    </Tabs>
-                </div>
-            </li>
-        )
-    }
+                    }
+
+                    <Tab eventKey="datasets" title="Datasets">
+                        <Container className={`mt-3 table--derivedDataset`}>
+                            <Table>
+                                <thead>
+                                <tr>
+                                    <th>SenNet ID</th>
+                                    <th>Data Types</th>
+                                    <th>Status</th>
+                                    <th>Derived Dataset Count</th>
+                                    <th>Last Modified</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {data.descendants.map((descendant_data, index) => {
+                                    if (descendant_data.entity_type === 'Dataset') {
+                                        return (
+                                            <React.Fragment key={index}>
+                                                <DerivedDatasetItem index={index} data={data}
+                                                                    data_type="Dataset"
+                                                                    descendant_uuid={descendant_data.uuid}/>
+                                            </React.Fragment>
+                                        )
+                                    }
+                                })}
+                                </tbody>
+                            </Table>
+                        </Container>
+                    </Tab>
+                </Tabs>
+            </div>
+        </li>
+    )
 }
+
+export default DerivedDataset
