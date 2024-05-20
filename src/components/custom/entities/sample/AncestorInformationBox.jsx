e2a1c28729370976aca7b680de23c9cc07cc088a
Lisa-Ann B <lisabruney@pitt.edu>
2024-05-20 15:02:39 -0400

    Converted AncestorInformationBox - #1416

--- src/components/custom/entities/sample/AncestorInformationBox.jsx
+++ src/components/custom/entities/sample/AncestorInformationBox.jsx
@@ -4,15 +4,15 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import SenNetAccordion from "../../layout/SenNetAccordion";
 import ClipboardCopy from "../../../ClipboardCopy";
 
-export default class AncestorInformationBox extends React.Component {
-    render() {
+const AncestorInformationBox = ({ ancestor }) => {
+
         return (
             <SenNetAccordion className={'mb-3'} title={'Ancestor'}>
                 <Card border={'0'} className={'pb-3'}>
                     <Card.Body>
                         <Card.Subtitle>
-                            <a href={'/' + this.props.ancestor.entity_type.toLowerCase() + '?uuid=' + this.props.ancestor.uuid}
-                               className="icon_inline mt-1">{this.props.ancestor.sennet_id}</a><ClipboardCopy text={this.props.ancestor.sennet_id} title={'Copy SenNet ID {text} to clipboard'} />
+                            <a href={'/' + ancestor.entity_type.toLowerCase() + '?uuid=' + ancestor.uuid}
+                               className="icon_inline mt-1">{ancestor.sennet_id}</a><ClipboardCopy text={ancestor.sennet_id} title={'Copy SenNet ID {text} to clipboard'} />
                         </Card.Subtitle>
                     </Card.Body>
                 </Card>
@@ -21,60 +21,62 @@ export default class AncestorInformationBox extends React.Component {
                     <Card border={'0'} className={'pb-3'}>
                         <Card.Body>
                             <Card.Subtitle>Ancestor Type</Card.Subtitle>
-                            <Card.Text>{this.props.ancestor.entity_type}</Card.Text>
+                            <Card.Text>{ancestor.entity_type}</Card.Text>
                         </Card.Body>
                     </Card>
 
-                    {this.props.ancestor.lab_source_id &&
+                    {ancestor.lab_source_id &&
                         <Card border={'0'} className={'pb-3'}>
                             <Card.Body>
                                 <Card.Subtitle>Lab ID</Card.Subtitle>
-                                <Card.Text>{this.props.ancestor.lab_source_id}</Card.Text>
+                                <Card.Text>{ancestor.lab_source_id}</Card.Text>
                             </Card.Body>
                         </Card>
                     }
-                    {this.props.ancestor.lab_tissue_sample_id &&
+                    {ancestor.lab_tissue_sample_id &&
                         <Card border={'0'} className={'pb-3'}>
                             <Card.Body>
                                 <Card.Subtitle>Lab ID</Card.Subtitle>
-                                <Card.Text>{this.props.ancestor.lab_tissue_sample_id}</Card.Text>
+                                <Card.Text>{ancestor.lab_tissue_sample_id}</Card.Text>
                             </Card.Body>
                         </Card>
                     }
-                    {this.props.ancestor.source_type &&
+                    {ancestor.source_type &&
                         <Card border={'0'} className={'pb-3'}>
                             <Card.Body>
                                 <Card.Subtitle>Source Type</Card.Subtitle>
-                                <Card.Text>{this.props.ancestor.source_type}</Card.Text>
+                                <Card.Text>{ancestor.source_type}</Card.Text>
                             </Card.Body>
                         </Card>
                     }
-                    {this.props.ancestor.sample_category &&
+                    {ancestor.sample_category &&
                         <Card border={'0'} className={'pb-3'}>
                             <Card.Body>
                                 <Card.Subtitle>Sample Category</Card.Subtitle>
-                                <Card.Text>{this.props.ancestor.sample_category}</Card.Text>
+                                <Card.Text>{ancestor.sample_category}</Card.Text>
                             </Card.Body>
                         </Card>
                     }
-                    {this.props.ancestor.organ &&
+                    {ancestor.organ &&
                         <Card border={'0'} className={'pb-3'}>
                             <Card.Body>
                                 <Card.Subtitle>Organ Type</Card.Subtitle>
-                                <Card.Text>{this.props.ancestor.display_subtype}</Card.Text>
+                                <Card.Text>{ancestor.display_subtype}</Card.Text>
                             </Card.Body>
                         </Card>
                     }
-                    {this.props.ancestor.group_name &&
+                    {ancestor.group_name &&
                         <Card border={'0'} className={'pb-3'}>
                             <Card.Body>
                                 <Card.Subtitle>Group Name</Card.Subtitle>
-                                <Card.Text>{this.props.ancestor.group_name}</Card.Text>
+                                <Card.Text>{ancestor.group_name}</Card.Text>
                             </Card.Body>
                         </Card>
                     }
                 </CardGroup>
             </SenNetAccordion>
         )
-    }
+
 }
+
+export default AncestorInformationBox
