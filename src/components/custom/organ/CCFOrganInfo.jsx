77bf4ec3e5a919692db7a17b25c7a2c9a611d709
maxsibilla <maxsibilla@icloud.com>
2024-08-12 09:52:11 -0400

    Updating data-sources component to get HRA to acknowledge SenNet session token

--- src/components/custom/organ/CCFOrganInfo.jsx
+++ src/components/custom/organ/CCFOrganInfo.jsx
@@ -1,11 +1,10 @@
 import {Helmet, HelmetProvider} from 'react-helmet-async'
-import {useEffect, useRef} from "react";
 import {getCookie} from "cookies-next";
-import {getAssetsEndpoint} from "@/config/config";
 
 const CCFOrganInfo = ({uberonUrl}) => {
     // See https://github.com/hubmapconsortium/hra-ui/blob/47490b8b5977def6cbaed047ebda6beb9e90fb97/EMBEDDING.md?plain=1#L412
-
+    console.log("https://apps.humanatlas.io/api/ds-graph/sennet?token=")
+    console.log(getCookie('groups_token'))
     // This overrides some styles in the ccf-organ-info. Allows the organ view to expand to the full width of the parent.
     const customStyles =
         '.btn {line-height: inherit !important;} .container {max-width: 100% !important;}'
@@ -23,20 +22,13 @@ const CCFOrganInfo = ({uberonUrl}) => {
             </Helmet>
             <ccf-organ-info
                 organ-iri={uberonUrl}
-                hubmap-data-service='search-api'
-                hubmap-portal-url='https://data.sennetconsortium.org/search'
-                hubmap-data-url='https://search.api.sennetconsortium.org/search'
-                hubmap-asset-url={getAssetsEndpoint()}
-                use-remote-api='true'
-                donor-label='Sources'
-                token={getCookie('groups_token')}
-                remote-api-endpoint="https://apps.humanatlas.io/api"
-                eui-url='https://data.sennetconsortium.org/ccf-eui'
-                rui-url='https://apps.humanatlas.io/rui'
-                asctb-url='https://hubmapconsortium.github.io/ccf-asct-reporter/'
-                hra-portal-url='https://humanatlas.io/'
-                online-course-url='https://expand.iu.edu/browse/sice/cns/courses/hubmap-visible-human-mooc'
-                paper-url='https://www.nature.com/articles/s41556-021-00788-6'
+                data-sources={`["https://apps.humanatlas.io/api/ds-graph/sennet?token=${getCookie('groups_token')}"]`}
+                eui-url="https://data.sennetconsortium.org/ccf-eui"
+                rui-url="https://apps.humanatlas.io/rui/"
+                asctb-url="https://hubmapconsortium.github.io/ccf-asct-reporter/"
+                hra-portal-url="https://humanatlas.io/"
+                online-course-url="https://expand.iu.edu/browse/sice/cns/courses/hubmap-visible-human-mooc"
+                paper-url="https://www.nature.com/articles/s41556-021-00788-6"
             />
         </HelmetProvider>
     )
