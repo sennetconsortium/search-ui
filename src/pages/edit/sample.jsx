256ce3d0edea8b5c7fc2711bab59f91396cb4ce9
Lisa-Ann B <lisabruney@pitt.edu>
2024-08-29 14:51:52 -0400

    REmove ancestor organs call that caused the error on entity with organ - #1618

--- src/pages/edit/sample.jsx
+++ src/pages/edit/sample.jsx
@@ -7,7 +7,7 @@ import Form from 'react-bootstrap/Form';
 import {Layout} from "@elastic/react-search-ui-views";
 import log from "loglevel";
 import {cleanJson, eq, fetchEntity, getDOIPattern, getRequestHeaders} from "../../components/custom/js/functions";
-import {get_ancestor_organs, getAncestryData, getEntityData, parseJson, update_create_entity} from "../../lib/services";
+import {getAncestryData, getEntityData, parseJson, update_create_entity} from "../../lib/services";
 import AppContext from '../../context/AppContext'
 import EntityContext, {EntityProvider} from '../../context/EntityContext'
 import {getUserName, isRuiSupported} from "../../config/config";
@@ -157,8 +157,7 @@ function EditSample() {
                     await fetchSource(_data.immediate_ancestors[0].uuid);
                 }
 
-                let ancestor_organ = await get_ancestor_organs(_data.uuid)
-                setAncestorOrgan(ancestor_organ)
+                setAncestorOrgan(_data.organ ? [_data.organ] : [_data?.origin_sample.organ])
                 setAncestorSource([getSourceType(_data.source)])
 
                 if (_data['rui_location'] !== undefined) {
