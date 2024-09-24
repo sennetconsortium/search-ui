3a973c8d694b1b5c32fe36ec1a0f7134b42fb600
maxsibilla <maxsibilla@icloud.com>
2024-09-24 10:27:37 -0400

    Updating ancestor/descendant tables to utilize getDatasetTypeDisplay function

--- src/components/custom/entities/sample/Lineage.jsx
+++ src/components/custom/entities/sample/Lineage.jsx
@@ -41,7 +41,6 @@ const Lineage = ({ lineage }) => {
     const data = [];
     {
         lineage.map((lineage_data, index) => {
-            console.log(lineage_data)
             data.push({
                 sennet_id: <span className={'has-supIcon'}><a href={'/' + lineage_data.entity_type.toLowerCase() + '?uuid=' + lineage_data.uuid}
                                                               className="icon_inline">{lineage_data.sennet_id}</a><ClipboardCopy text={lineage_data.sennet_id} size={10} title={'Copy SenNet ID {text} to clipboard'} /></span>,
