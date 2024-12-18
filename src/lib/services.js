2d0363af2e05c99d5ff873e065313472134f1817
Lisa-Ann B <lisabruney@pitt.edu>
2024-12-18 09:09:39 -0500

    Generalized method name - #1780

--- src/lib/services.js
+++ src/lib/services.js
@@ -131,7 +131,7 @@ export async function get_read_write_privileges() {
     }
 }
 
-export async function call_privs_service(path, base='privs/') {
+export async function call_ingest_service(path, base='privs/') {
     const url = getIngestEndPoint() + base + path;
     const request_options = {
         method: 'GET',
@@ -154,22 +154,22 @@ export async function call_privs_service(path, base='privs/') {
 
 export async function has_data_admin_privs() {
     log.debug('FETCHING DATA ADMIN PRIVS')
-    return await call_privs_service('has-data-admin')
+    return await call_ingest_service('has-data-admin')
 }
 
 export async function get_write_privilege_for_group_uuid(group_uuid) {
     log.debug('GET WRITE PRIVILEGE FOR GROUP UUID')
-    return await call_privs_service(group_uuid + '/has-write')
+    return await call_ingest_service(group_uuid + '/has-write')
 }
 
 export async function get_user_write_groups() {
     log.debug('FETCHING USER WRITE GROUPS')
-    return await call_privs_service('user-write-groups')
+    return await call_ingest_service('user-write-groups')
 }
 
 export async function get_provider_groups() {
     log.debug('FETCHING Provider GROUPS')
-    return await call_privs_service('data-provider-groups', 'metadata/')
+    return await call_ingest_service('data-provider-groups', 'metadata/')
 }
 
 
